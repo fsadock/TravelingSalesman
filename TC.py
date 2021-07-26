03e52bb8669ff36cf0cac8442c5a199ec6afee82
Felipe Sadock <fsadock@gmail.com>
2021-07-26 16:25:33 -0300

    added progress bar, fixed some bugs

--- TC.py
+++ TC.py
@@ -3,6 +3,7 @@ import numpy as np
 import multiprocessing as mp
 import matplotlib.pyplot as plt
 import time
+import tqdm
 
 
 # generates a random list of integers between (including) _a_ and _b_
@@ -54,28 +55,25 @@ def shake(x,k):
     return y
 
 # Returns the first improvement after permutating combinations of x - an array of route 
-# Using for loops I can control which permutation has already been calculated. Using something like shuffle could be better,
-    #  but when getting closer to the minimum value it could take forever to find another improvement.
 def firstImprovement(x, k, matriz):
     y = x[:]
     if k == 1:
         for i in range(len(y)):
-            for j in range(i, len(y)):
-                if i != j:
-                    y[i], y[j] = y[j], y[i]
-                    if fobj(y, matriz) < fobj(x, matriz): # if y is better than x, return y ( FIRST IMPROVEMENT )
-                        return y
-                    y = x[:]                              # else: sets y = x to proceed to the next permutation
-    elif k == 2:                            # k == 2 NEEDS TO BE REIMPLEMENTED, just awful.
+            for j in range(i+1, len(y)):
+                y[i], y[j] = y[j], y[i]
+                if fobj(y, matriz) < fobj(x, matriz): # if y is better than x, search y ( FIRST IMPROVEMENT )
+                    return firstImprovement(y, k=1, matriz=matriz)
+                continue                    # if not better, continue searching the neighborhood until nothing is found                            
+    elif k == 2:
         for i in range(len(y)):
             for j in range(i, len(y)):
                 for u in range(j, len(y)):
                     if i != j and u!=j and i!=u:
                         y[i], y[j], y[u] = y[j], y[u], y[i]
                         if fobj(y, matriz) < fobj(x, matriz):
-                            return y
-                        y = x[:]
-    return x                                  # if no other permutation is better than the original, returns x
+                            return firstImprovement(y, k=2, matriz=matriz)
+                        continue
+    return x                                  # if no other permutation is better return x
 
 # Variable Neighborhood Descent
 def VND(x , k_max, matriz):
@@ -89,78 +87,84 @@ def VND(x , k_max, matriz):
 def rvns(x, k_max, t_max, matriz):
     t = 0
     results = []
+    pbar = tqdm.tqdm(total=t_max)
     while t<=t_max:
         k = 1
         while k <= k_max:
             x_lin = shake(x,k)
             t+=1
+            pbar.update(1)
             x, k = neighborhoodChange(x, x_lin, k, matriz)
         results.append(np.round(fobj(x, matriz)))
+    pbar.close()
     return x, results
 
 # Basic Variable Neighborhood Search
 def bvns(x, k_max, t_max, matriz):
     t = 0
     results = []
+    pbar = tqdm.tqdm(total=t_max)
     while t<=t_max:
-        print(f"### --------------- {t} --------------- ###")
         k = 1
         while k <= k_max:
             x_lin = shake(x,k)
             x_dlin = firstImprovement(x_lin, k, matriz)
             x, k = neighborhoodChange(x, x_dlin, k, matriz)
         t+=1
+        pbar.update(1)
         results.append(np.round(fobj(x, matriz)))
+    pbar.close()
     return x, results
 
 # General Variable Neighborhood Search
 def gvns(x,l_max, k_max, t_max, matriz):
     t = 0
     results = []
+    pbar = tqdm.tqdm(total=t_max)
     while t<=t_max:
-        print(f"\n### --------------- {t} --------------- ###")
         k = 1
         while k <= k_max:
             x_lin = shake(x,k)
             x_dlin = VND(x_lin, l_max, matriz)
             x, k = neighborhoodChange(x, x_dlin, k, matriz)
         t+=1
+        pbar.update(1)
         results.append(np.round(fobj(x, matriz)))
+    pbar.close()
     return x, results
 
+def main():
 
-arquivo = "time.csv"
-matriz0 = np.genfromtxt(arquivo, delimiter=',') # uses numpy to read the matrix from the csv file
+    arquivo = "time.csv"
+    matriz0 = np.genfromtxt(arquivo, delimiter=',') # uses numpy to read the matrix from the csv file
 
-# a smaller distance matrix
-matriz1 = [
-        [0, 2451, 713, 1018, 1631, 1374, 2408, 213, 2571, 875, 1420, 2145, 1972],
-        [2451, 0, 1745, 1524, 831, 1240, 959, 2596, 403, 1589, 1374, 357, 579],
-        [713, 1745, 0, 355, 920, 803, 1737, 851, 1858, 262, 940, 1453, 1260],
-        [1018, 1524, 355, 0, 700, 862, 1395, 1123, 1584, 466, 1056, 1280, 987],
-        [1631, 831, 920, 700, 0, 663, 1021, 1769, 949, 796, 879, 586, 371],
-        [1374, 1240, 803, 862, 663, 0, 1681, 1551, 1765, 547, 225, 887, 999],
-        [2408, 959, 1737, 1395, 1021, 1681, 0, 2493, 678, 1724, 1891, 1114, 701],
-        [213, 2596, 851, 1123, 1769, 1551, 2493, 0, 2699, 1038, 1605, 2300, 2099],
-        [2571, 403, 1858, 1584, 949, 1765, 678, 2699, 0, 1744, 1645, 653, 600],
-        [875, 1589, 262, 466, 796, 547, 1724, 1038, 1744, 0, 679, 1272, 1162],
-        [1420, 1374, 940, 1056, 879, 225, 1891, 1605, 1645, 679, 0, 1017, 1200],
-        [2145, 357, 1453, 1280, 586, 887, 1114, 2300, 653, 1272, 1017, 0, 504],
-        [1972, 579, 1260, 987, 371, 999, 701, 2099, 600, 1162, 1200, 504, 0],
-    ]
+    # a smaller distance matrix
+    matriz1 = [
+            [0, 2451, 713, 1018, 1631, 1374, 2408, 213, 2571, 875, 1420, 2145, 1972],
+            [2451, 0, 1745, 1524, 831, 1240, 959, 2596, 403, 1589, 1374, 357, 579],
+            [713, 1745, 0, 355, 920, 803, 1737, 851, 1858, 262, 940, 1453, 1260],
+            [1018, 1524, 355, 0, 700, 862, 1395, 1123, 1584, 466, 1056, 1280, 987],
+            [1631, 831, 920, 700, 0, 663, 1021, 1769, 949, 796, 879, 586, 371],
+            [1374, 1240, 803, 862, 663, 0, 1681, 1551, 1765, 547, 225, 887, 999],
+            [2408, 959, 1737, 1395, 1021, 1681, 0, 2493, 678, 1724, 1891, 1114, 701],
+            [213, 2596, 851, 1123, 1769, 1551, 2493, 0, 2699, 1038, 1605, 2300, 2099],
+            [2571, 403, 1858, 1584, 949, 1765, 678, 2699, 0, 1744, 1645, 653, 600],
+            [875, 1589, 262, 466, 796, 547, 1724, 1038, 1744, 0, 679, 1272, 1162],
+            [1420, 1374, 940, 1056, 879, 225, 1891, 1605, 1645, 679, 0, 1017, 1200],
+            [2145, 357, 1453, 1280, 586, 887, 1114, 2300, 653, 1272, 1017, 0, 504],
+            [1972, 579, 1260, 987, 371, 999, 701, 2099, 600, 1162, 1200, 504, 0],
+        ]
 
-# another test matrix
-np.random.seed(42)
-matriz2 = np.random.randint(30, 2000, (30,30))
+    # another test matrix
+    np.random.seed(42)
+    matriz2 = np.random.randint(0, 30, (30,30))
 
-# sets the matrix you want to solve
-matriz = matriz0
-print("\nChosen Matrix:")
-print(matriz)
-print('\n')
+    # sets the matrix you want to solve
+    matriz = matriz0
 
-if __name__ == '__main__':
-
+    print("\nChosen Matrix:")
+    print(matriz)
+    print('\n')
     start_time = time.time()
     x = initialSol(0,len(matriz))    
     # print(x)
@@ -173,18 +177,23 @@ if __name__ == '__main__':
 #### --------------------------------------------------------- ####
 
     # Reduced VNS takes a lot less computational time but does not give the best results
-    # sol, results = rvns(x,k_max=4,t_max=1000000, matriz=matriz)
-
-    sol, results = gvns(x,l_max=1,k_max=3,t_max=10, matriz=matriz)
+    # sol, results = bvns(x,k_max=4,t_max=1000, matriz=matriz)
+    print(fobj(x, matriz))
+    print()
+    sol, results = gvns(x,l_max=2,k_max=3,t_max=100, matriz=matriz)
     print(sol)
     print()
-    print(results)
+    print(results[-1])
     
     print(f"--- {time.time() - start_time} seconds ---")
     
     plt.plot(results)
     # plt.plot(resultsParallel[0][1])
-    plt.show()
+    # plt.show()
+
+if __name__ == '__main__':
+    main()
+    
    
 
 
